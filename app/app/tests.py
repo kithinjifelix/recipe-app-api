eb37645a60d4ad622775e72902ba0ae6ed7c405e
felixkithinji <felixkithinji@outlook.com>
2025-02-13 15:38:11 +0300

    fixing tests.py linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,12 +10,10 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """ Test the calc module."""
 
-
     def test_add_numbers(self):
         """ Test that two numbers are added together """
         res = calc.add(3, 8)
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """ Test that values are subtracted and returned """
