f0eb5164a909eadccccea4b7893043896abd7a38
felixkithinji <felixkithinji@outlook.com>
2025-02-22 11:30:23 +0300

    Correct linting problems

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,6 +10,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """
@@ -23,7 +24,6 @@ class CommandTests(SimpleTestCase):
         patched_check.return_value = True
 
         call_command('wait_for_db')
-        
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
